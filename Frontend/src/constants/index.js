181d6cb8ce29bc487f144f687e8776aeae5c4706
Gfx-Boy <ahsgaming786@gmail.com>
2024-08-13 16:08:31 +0500

    first commit

--- Frontend/src/constants/index.js
+++ Frontend/src/constants/index.js
@@ -142,7 +142,7 @@ export const stats = [
   {
     id: "stats-1",
     title: "Trusted Clients",
-    value: "1000+",
+    value: "100+",
   },
   {
     id: "stats-2",
@@ -158,32 +158,7 @@ export const stats = [
 
 export const footerLinks = [
   {
-    title: "Useful Links",
-    links: [
-      {
-        name: "Content",
-        link: "https://www.hoobank.com/content/",
-      },
-      {
-        name: "How it Works",
-        link: "https://www.hoobank.com/how-it-works/",
-      },
-      {
-        name: "Create",
-        link: "https://www.hoobank.com/create/",
-      },
-      {
-        name: "Explore",
-        link: "https://www.hoobank.com/explore/",
-      },
-      {
-        name: "Terms & Services",
-        link: "https://www.hoobank.com/terms-and-services/",
-      },
-    ],
-  },
-  {
-    title: "Community",
+    title: "Contact",
     links: [
       {
         name: "Help Center",
@@ -206,20 +181,7 @@ export const footerLinks = [
         link: "https://www.hoobank.com/newsletters/",
       },
     ],
-  },
-  {
-    title: "Partner",
-    links: [
-      {
-        name: "Our Partner",
-        link: "https://www.hoobank.com/our-partner/",
-      },
-      {
-        name: "Become a Partner",
-        link: "https://www.hoobank.com/become-a-partner/",
-      },
-    ],
-  },
+  }
 ];
 
 export const socialMedia = [
